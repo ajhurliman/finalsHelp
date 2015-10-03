a1e6071e596df4fb83e0713a21374fbcdc5ea3f2
ajHurliman <james.hurliman@gmail.com>
2015-10-02 20:30:47 -0700

    added app dynamics
    Former-commit-id: 678334aca112cf2f145b76ff511c337c51bc1df3

--- server.js
+++ server.js
@@ -1,5 +1,15 @@
 'use strict';
 
+require("appdynamics").profile({
+    controllerHostName: 'www.finalshelp.com',
+    controllerPort: 80, // If SSL, be sure to enable the next line  
+    accountName: 'FinalsHelpcom', // Required for a controller running in multi-tenant mode
+    accountAccessKey: 'ml8a3qr7o9oi', // Required for a controller running in multi-tenant mode
+    applicationName: 'FinalsHelp',
+    tierName: 'tier1',
+    nodeName: 'process' // The controller will automatically append the node name with a unique number
+});
+
 var compress    = require( 'compression' );
 var bodyParser  = require( 'body-parser' );
 var passport    = require( 'passport' );
