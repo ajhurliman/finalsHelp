989d26643f74a10d5234d764767dc6ecee91d15c
ajHurliman <james.hurliman@gmail.com>
2015-10-23 18:15:37 -0700

    debug
    Former-commit-id: df68b89125b006b8d16f7c0326e13850e59d45eb

---
+++ build/wsu/src/components/services/FindImageService/FindImageService.js
@@ -0,0 +1,58 @@
+angular.module('fh.services.FindImageService', [
+        'ngStorage',
+        'vendor.steelToe'
+    ])
+
+.factory('FindImageService', function($http, $sessionStorage, $q, steelToe) {
+
+    function isImage(src, defaultSrc) {
+
+        var deferred = $q.defer();
+
+        var image = new Image();
+        image.onerror = function() {
+            // console.log('error: ' + src + ' not found');
+            deferred.resolve( defaultSrc );
+        };
+        image.onload = function() {
+            deferred.resolve( src );
+        };
+        image.src = src;
+
+        return deferred.promise;
+    }
+
+    return {
+        getHeaderImage: function(companyCode) {
+            var imageUrl = './assets/images/headerImage.jpg';
+            return isImage(imageUrl);
+        }
+    };
+});
+
+
+
+// interior
+// I, J, K, L, M, MM, N, NN, IA, IQ, R
+
+// ocean
+// C, CA, CQ, D, DA, DD, E, EE, F, FA, FB, FF, G, H, HH, GG, OO, Q
+
+// vista
+// A, AA, AB, AS, B, BA, BB, BC, BQ
+
+// neptune
+// S, SA, SB, SC, SQ
+
+// pinnacle
+// PS
+
+// verandah
+// V, VA, VB, VC, VD, VE, VF, VH, VQ, VS, VT
+
+// signature
+// SS, SY, SZ, SU
+
+// lanai
+// CA
+
